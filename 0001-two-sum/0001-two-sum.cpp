5c8b028212eeeb25e13a1dd8687918832578e0a1
Gaurang Sharma <gaurangsharma2006@gmail.com>
2025-07-31 08:39:34 +0530

    Time: 0 ms (100%), Space: 14.7 MB (58.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int , int > psf ; 
-        for(int i = 0 ; i < nums.size(); i++)
+        for(int i = 0 ; i < nums.size() ; i++)
         {
-            int a = nums[i];
-            int rem = target - a ; 
+            int k = nums[i] ; 
+            int rem = target - k ; 
             if(psf.find(rem) != psf.end())
             {
-                return {psf[rem] , i}; 
+                return {psf[rem] , i };
             }
-            psf[a] = i  ; 
+            psf[k] = i ; 
         }
-       return {}; 
+        return {};
     }
 };
