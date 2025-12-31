0bbb8c0ab7872b9add7dae2b1f970ee992b5c316
Gaurang Sharma <gaurangsharma2006@gmail.com>
2026-01-01 00:05:04 +0530

    Time: 0 ms (100%), Space: 18.6 MB (59.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,6 +4,7 @@ class Solution:
         for i , n in enumerate (nums):
             diff = target - n 
             if diff in psm:
-                return [psm[diff], i]
-            psm[n] = i     
+                return[psm[diff],i]
+            psm[n] = i    
+
         
