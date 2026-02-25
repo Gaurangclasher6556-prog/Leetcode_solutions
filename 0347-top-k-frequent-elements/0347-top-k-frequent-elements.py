1365975d6d2e55b3a7fb133304cece8f99aede05
Gaurang Sharma <gaurangsharma2006@gmail.com>
2026-02-25 11:02:32 +0530

    Time: 6 ms (57.51%), Space: 22.9 MB (51.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
-        # Use nlargest() to get top k by frequency
-        return [item for item, count in heapq.nlargest(k, freq.items(), key=lambda x: x[1])]
+        
+        sorted(freq)
+        return [item[0] for item in freq.most_common(k)]
