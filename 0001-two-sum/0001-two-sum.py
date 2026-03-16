8305ee831d930bddce225451adaf378564fc6adb
Gaurang Sharma <gaurangsharma2006@gmail.com>
2026-03-16 10:14:55 +0530

    Time: 0 ms (100%), Space: 20.4 MB (64.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         psm = {}
-        for i , n in enumerate (nums):
+        for i,n in enumerate (nums):
             diff = target - n 
             if diff in psm:
-                return[psm[diff],i]
-            psm[n] = i    
+                return [psm[diff],i]
+            psm[n] = i 
 
-        
